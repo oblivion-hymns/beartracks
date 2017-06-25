ce44cf2495fbe23286d5e3607f3d5d6cc037ea9f
Brian Wilbur <brian@bwilbur.com>
2017-06-25 11:18:13 -0400

    Updating navbar layout to be ultra pretty

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -5,46 +5,7 @@ import { MessageService } from './messages/message.service';
 @Component({
 	providers: [MessageService],
 	selector: 'my-app',
-	styles: [`
-		#PageContent {
-			padding: 50px;
-		}
-
-		md-toolbar {
-			background-color: rgb(40, 40, 40);
-		}
-
-		[md-button] {
-			text-transform: lowercase;
-		}
-
-		md-toolbar [md-button] {
-			color: rgba(255, 255, 255, 0.54);
-			height: 100%;
-			padding: 16px 32px;
-			width: 16%;
-		}
-
-		md-sidenav-container {
-			background-color: rgb(27, 27, 27);
-			height: 100%;
-		}
-
-		md-sidenav {
-			padding: 8px;
-			width: 256px;
-		}
-
-		md-sidenav button {
-			display: inline-block;
-			text-align: left;
-			width: 100%;
-		}
-
-		md-sidenav button md-icon {
-			margin-right: 6px;
-		}
-	`],
+	styleUrls: ['./app.component.css'],
 	templateUrl: './app.component.html'
 })
 export class AppComponent {
