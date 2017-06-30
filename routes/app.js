f3b5a4c1c3c8e43fd45e0bb408244a08b42f57a0
Brian Wilbur <brian@bwilbur.com>
2017-06-30 07:02:59 -0400

    Added file path caching

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var extend = require('util')._extend;
 var fs = require('fs');
 var id3 = require('node-id3');
-var recursiveReaddirSync = require('recursive-readdir-sync');
 var path = require('path');
+var recursiveReaddirSync = require('recursive-readdir-sync');
 
 var Artist = require('../models/artist');
 var Album = require('../models/album');
@@ -36,46 +37,67 @@ function sync(req, res)
 
 	console.log('Scanning files for data...');
 
+	var cache = {};
+	var cacheFilePath = null;
+
 	//Prune for artists
 	for (var i in files)
 	{
 		var file = files[i];
 		var fileType = path.extname(file);
 
-		if (file.indexOf('.cache.json') > -1)
+		var isCache = (file.indexOf('.cache.json') > -1);
+		if (isCache)
 		{
-			console.log('Found cache');
+			cacheFilePath = file;
+			var cacheFile = require(cacheFilePath);
+			cache = extend(cache, cacheFile);
 		}
 
-		var isAlbumArt = false;
-		var isArtistArt = false;
-		var isSong = false;
-		var filePath = file;
-		var tags = null;
-
-		if (fileType == '.mp3')
+		var cached = cache.files.indexOf(encodeURIComponent(file)) != -1;
+		if (!cached)
 		{
-			tags = id3.read(file);
-			isSong = true;
-		}
-		else if (file.indexOf('folder.png') > -1)
-		{
-			isAlbumArt = true;
-		}
-		else if (file.indexOf('artist.png') > -1)
-		{
-			isArtistArt = true;
-		}
+			var isAlbumArt = false;
+			var isArtistArt = false;
+			var isSong = false;
+			var filePath = file;
+			var tags = null;
 
-		console.log('Adding ' + filePath);
+			if (fileType == '.mp3')
+			{
+				tags = id3.read(file);
+				isSong = true;
+			}
+			else if (file.indexOf('folder.png') > -1)
+			{
+				isAlbumArt = true;
+			}
+			else if (file.indexOf('artist.png') > -1)
+			{
+				isArtistArt = true;
+			}
 
-		allData.push({
-			isAlbumArt: isAlbumArt,
-			isArtistArt: isArtistArt,
-			isSong: isSong,
-			path: filePath,
-			tags: tags
-		});
+			console.log('Adding ' + filePath);
+
+			allData.push({
+				isAlbumArt: isAlbumArt,
+				isArtistArt: isArtistArt,
+				isSong: isSong,
+				path: filePath,
+				tags: tags
+			});
+
+			cache.files.push(encodeURIComponent(file));
+			var json = JSON.stringify(cache);
+			fs.writeFile(cacheFilePath, json, 'utf8', function(){});
+		}
+		else
+		{
+			if (!isCache)
+			{
+				console.log('Skipping ' + file);
+			}
+		}
 	}
 
 	var dataByArtist = {};
