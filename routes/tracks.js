f16e33fffc4cfa282d8c37ff1c70ddf305a52c71
Brian Wilbur <brian@bwilbur.com>
2017-07-02 16:31:45 -0400

    Track routes

---
+++ routes/tracks.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Track = require('../models/track');
+
+var router = express.Router();
+router.get('/', baseRoute);
+router.get('/all', loadAll);
+
+function baseRoute(req, res)
+{
+	res.render('index');
+}
+
+function loadAll(req, res)
+{
+	Track.find({}).sort('discNum, trackNum').populate('album, album.artist').exec(function(error, albums){
+		if (error)
+		{
+			console.log(error);
+			return res.status(500).json({
+				message: 'An error occurred'
+			});
+		}
+
+		res.status(200).json({
+			albums: albums
+		});
+	});
+}
+
+module.exports = router;
