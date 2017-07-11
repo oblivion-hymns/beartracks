9de847270e4bf18381ef90ea2a3d20fa04cbe9a9
Brian Wilbur <brian@bwilbur.com>
2017-07-10 22:25:37 -0400

    SearchComponent Shell

---
+++ assets/app/search/search.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Artist } from './../artists/artist';
+import { Album } from './../albums/album';
+import { Track } from './../track/track';
+import { ArtistService } from './../artists/artist.service';
+import { AlbumService } from './../albums/album.service';
+import { TrackService } from './../track/track.service';
+
+@Component({
+	providers: [ArtistService, AlbumService, TrackService],
+	selector: 'bt-search',
+	styles: [``],
+	templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit {
+	artists: Artist[] = [];
+	albums: Album[] = [];
+	tracks: Track[] = [];
+	filterQuery: string = '';
+
+	constructor(private artistService: ArtistService,
+				private albumService: AlbumService,
+				private trackService: TrackService) {}
+
+	ngOnInit()
+	{
+
+	}
+
+	set filterString(value)
+	{
+
+	}
+}
