44f22f9b68065bf30d377ac3bfb92b8f8f3a8839
Brian Wilbur <brian@bwilbur.com>
2017-07-14 17:43:03 -0400

    Workflow page

--- assets/app/artists/artists.component.ts
+++ assets/app/artists/artists.component.ts
@@ -2,57 +2,75 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Artist } from './artist';
 import { ArtistService } from './artist.service';
+import { Album } from './../albums/album';
+import { AlbumService } from './../albums/album.service';
+import { Track } from './../tracks/track';
+import { TrackService } from './../tracks/track.service';
 
 @Component({
-	providers: [ArtistService],
+	providers: [ArtistService, AlbumService, TrackService],
 	selector: 'bt-artists',
 	styles: [`
-		#FilterArtists input {
-			color: rgba(255, 255, 255, 0.54);
+		.item-line
+		{
+			padding: 5px;
+			overflow: hidden;
+		}
+
+		.item-line:hover
+		{
+			background-color: rgba(40, 40, 40, 0.87);
+			cursor: pointer;
+		}
+
+		.item-line img
+		{
+			float: left;
+			height: 48px;
+			margin-right: 10px;
+			width: 48px;
+		}
+
+		.item-line .item-text
+		{
+			display: block;
+			line-height: 24px;
+			margin-top: 0;
+			white-space: normal;
+		}
+
+		.item-line .item-text-single
+		{
+			display: block;
+			line-height: 24px;
+			text-overflow: ellipsis;
+			overflow-x: hidden;
+		}
+
+		.item-line .item-text-single.item-text-description
+		{
+			color: rgba(255, 255, 255, 0.34);
 		}
 	`],
 	templateUrl: './artists.component.html'
 })
 export class ArtistsComponent implements OnInit {
 	artists: Artist[] = [];
-	displayArtists: Artist[] = [];
+	albums: Album[] = [];
+	tracks: Track[] = [];
+	loadingArtists: boolean = true;
+	loadingAlbums: boolean = false;
+	loadingTracks: boolean = false;
 
-	@Input() filterQuery = '';
-
-	constructor(private artistService: ArtistService) {}
+	constructor(private artistService: ArtistService,
+				private albumService: AlbumService,
+				private trackService: TrackService) {}
 
 	ngOnInit()
 	{
-		this.artistService.loadAll().subscribe(
-			(artists: Artist[]) => { this.artists = artists; }
-		)
-	}
-
-	set filterString(value)
-	{
-		this.displayArtists = [];
-
-		value = value.trim();
-		if (value == '*')
-		{
-			this.displayArtists = this.artists.filter(
-				artist => artist.nameKey.includes(''));
-		}
-		else
-		{
-			var transformedValue = value.toLowerCase().replace(/\W/g, '');
-			if (transformedValue.length > 2)
-			{
-				this.displayArtists = this.artists.filter(
-					artist => artist.nameKey.includes(transformedValue));
-			}
-		}
-
-		this.filterQuery = value;
-	}
-
-	filterArtists(query)
-	{
-		this.filterQuery = query;
+		this.artistService.loadAll().subscribe(function(artists: Artist[]){
+			this.artists = artists;
+			this.loadingArtists = false;
+		});
 	}
 }
