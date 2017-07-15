83bac9c7dc95f00f4d66b4dc798e63819a5499aa
Brian Wilbur <brian@bwilbur.com>
2017-07-14 21:05:20 -0400

    Surprise me button beginnings!

--- assets/app/jukebox/jukebox.component.ts
+++ assets/app/jukebox/jukebox.component.ts
@@ -29,7 +29,6 @@ export class JukeboxComponent implements AfterViewChecked
 	@ViewChild('chat') input;
 	username: string = 'User ' + (Math.floor(Math.random() * (1000000 - 0) + 0));
 
-
 	constructor(private jukeboxService: JukeboxService,
 		private artistService: ArtistService,
 		private albumService: AlbumService,
