7166a945a1e4f012a18aab668fc77c0e1bf976ee
Brian Wilbur <brian@bwilbur.com>
2017-07-22 20:50:52 -0400

    Find relate track via genre map

--- routes/tracks.js
+++ routes/tracks.js
@@ -6,12 +6,13 @@ var mongoose = require('mongoose');
 var Artist = require('../models/artist');
 var Album = require('../models/album');
 var Track = require('../models/track');
+var genreMap = require('../genres');
 
 var router = express.Router();
 router.get('/', baseRoute);
 router.get('/random', loadRandom);
-router.get('/all', loadAll);
 router.get('/find', find);
+router.post('/related', loadRelated);
 router.post('/album', loadAlbum);
 router.get('/increment-song', incrementSong);
 router.get('/recent', loadRecentlyPlayed);
@@ -21,6 +22,73 @@ function baseRoute(req, res)
 	res.render('index');
 }
 
+function getRelatedGenre(genre, degree)
+{
+	for (var i = 0; i < genreMap.genres.length; i++)
+	{
+		var genreInfo = genreMap.genres[i];
+		if (genre == genreInfo.name)
+		{
+			return genreInfo.related[Math.floor(Math.random() * genreInfo.related.length)];
+		}
+	}
+}
+
+function loadRelated(req, res)
+{
+	var trackId = mongoose.Types.ObjectId(req.query.trackId);
+	var degree = req.query.degree;
+
+	Track.find({_id: trackId}, function(error, data){
+
+		if (error)
+		{
+			console.error(error);
+			return res.status(500).json({
+				message: 'An error occurred'
+			});
+		}
+
+		var genre = data[0].genre;
+		for (var i = 0; i < degree; i++)
+		{
+			genre = getRelatedGenre(genre);
+		}
+
+		Track.count({genre: genre}).exec(function(error, count){
+			var random = Math.floor(Math.random() * count);
+			Track.findOne({genre: genre})
+				.skip(random)
+				.populate('album')
+				.populate({
+					path: 'album',
+					populate: {
+						path: 'artist',
+						model: 'Artist'
+					}
+				})
+				.exec(function(error, track){
+					if (error)
+					{
+						console.log(error);
+						return res.status(500).json({
+							message: 'An error occurred'
+						});
+					}
+
+					res.status(200).json({
+						track: track
+					});
+			});
+		});
+	});
+
+
+}
+
+/**
+ * Returns a completely randomly-selected track
+ */
 function loadRandom(req, res)
 {
 	Track.count().exec(function(error, count){
@@ -53,7 +121,7 @@ function loadRandom(req, res)
 }
 
 /**
- * Returns a list of albums with a name like the given one
+ * Returns a list of tracks with a name like the given one
  */
 function find(req, res)
 {
@@ -100,6 +168,9 @@ function find(req, res)
 	}
 }
 
+/**
+ * Loads and returns all tracks, in order, on the given album
+ */
 function loadAlbum(req, res)
 {
 	var id = mongoose.Types.ObjectId(req.body.albumId);
@@ -141,45 +212,6 @@ function loadAlbum(req, res)
 	});
 }
 
-function loadAll(req, res)
-{
-	Track.find({})
-		.populate('album')
-		.populate({
-			path: 'album',
-			populate: {
-				path: 'artist',
-				model: 'Artist'
-			}
-		})
-		.exec(function(error, tracks){
-			if (error)
-			{
-				console.log(error);
-				return res.status(500).json({
-					message: 'An error occurred'
-				});
-			}
-
-			tracks = tracks.sort(function(t1, t2){
-				var t1sortKey = t1.nameKey.replace(t1.album.nameKey, '');
-				var t2sortKey = t2.nameKey.replace(t2.album.nameKey, '');
-				if (t1sortKey > t2sortKey)
-				{
-					return 1;
-				}
-				else if (t1sortKey < t2sortKey)
-				{
-					return -1;
-				}
-			});
-
-			res.status(200).json({
-				tracks: tracks
-			});
-	});
-}
-
 /**
  * Increases the play count of a track by track ID
  */
@@ -198,6 +230,9 @@ function incrementSong(req, res)
 	});
 }
 
+/**
+ * Loads the most recently-played tracks
+ */
 function loadRecentlyPlayed(req, res)
 {
 	Track.find({playCount: {$gt: 0}})
