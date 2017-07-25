5b5b57982086cfd71063b3037a2242e4ef3074d7
Brian Wilbur <brian@bwilbur.com>
2017-07-25 19:45:53 -0400

    Tracks::loadAll() gone too

--- assets/app/tracks/track.service.ts
+++ assets/app/tracks/track.service.ts
@@ -220,35 +220,6 @@ export class TrackService
 			.catch((error: Response) => Observable.throw(error.json()));
 	}
 
-	loadAll()
-	{
-		return this.http.get('http://bwilbur.com/tracks/all')
-			.map((response: Response) => {
-				const data = response.json().tracks;
-
-				let tracks: Track[] = [];
-				for (let trackData of data)
-				{
-					var id = trackData._id;
-					var name = trackData.name;
-					var nameKey = trackData.nameKey;
-					var album = trackData.album;
-					var discNum = trackData.discNum;
-					var trackNum = trackData.trackNum;
-					var genre = trackData.genre;
-					var length = trackData.length;
-					var filePath = trackData.filePath;
-					var playCount = trackData.playCount;
-					var updatedAt = trackData.updatedAt;
-					var track = new Track(id, name, nameKey, album, discNum, trackNum, genre, length, filePath, playCount, updatedAt);
-					tracks.push(track);
-				}
-				this.tracks = tracks;
-				return tracks;
-			})
-			.catch((error: Response) => Observable.throw(error.json()));
-	}
-
 	/**
 	 * Returns the complete genre map
 	 */
