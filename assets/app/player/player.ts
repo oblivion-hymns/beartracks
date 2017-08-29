1bfa1ecd2d0c87aa22c410fc3074902f3e8f76a9
Brian Wilbur <brian@bwilbur.com>
2017-08-28 21:55:55 -0400

    FUCK YES

--- assets/app/player/player.ts
+++ assets/app/player/player.ts
@@ -189,8 +189,7 @@ export class Player
 			this.audio.preload = 'auto';
 			this.audio.load();
 			this.audio.oncanplaythrough = () => {
-				console.log('ready');
-				/*this.isLoading = false;
+				this.isLoading = false;
 				this.audio.play();
 				this.audio.volume = this.volume;
 				this.checkTimeInterval();
@@ -201,12 +200,9 @@ export class Player
 				this.pushNotifications.create('Now Playing', {
 					body: body,
 					icon: this.currentTrack.album.imagePath
-				}).subscribe();*/
+				}).subscribe();
 			};
 		});
-
-
-
 	}
 
 	/**
@@ -223,24 +219,27 @@ export class Player
 			this.currentTrack = this.queue[this.queuePosition];
 			this.homeGenre = this.currentTrack.genre;
 			this.isLoading = true;
-			this.audio = new Audio(this.currentTrack.filePath);
-			this.audio.load();
-			this.audio.oncanplaythrough = () => {
-				console.log('on can play through');
-				this.isLoading = false;
-				this.audio.play();
-				this.audio.volume = this.volume;
-			};
 
-			this.checkTimeInterval();
+			this.trackService.loadTrack(this.currentTrack.filePath).subscribe(blob => {
+				var blobPath = window.URL.createObjectURL(blob);
+				this.audio = new Audio(blobPath);
+				this.audio.preload = 'auto';
+				this.audio.load();
+				this.audio.oncanplaythrough = () => {
+					this.isLoading = false;
+					this.audio.play();
+					this.audio.volume = this.volume;
+					this.checkTimeInterval();
 
-			var body = this.currentTrack.album.artist.name + ' - "' + this.currentTrack.name + '"\n';
-			body += this.currentTrack.album.name + ' (' + this.currentTrack.album.year + ')';
+					var body = this.currentTrack.album.artist.name + ' - "' + this.currentTrack.name + '"\n';
+					body += this.currentTrack.album.name + ' (' + this.currentTrack.album.year + ')';
 
-			this.pushNotifications.create('Now Playing', {
-				body: body,
-				icon: this.currentTrack.album.imagePath
-			}).subscribe();
+					this.pushNotifications.create('Now Playing', {
+						body: body,
+						icon: this.currentTrack.album.imagePath
+					}).subscribe();
+				};
+			});
 		}
 		else
 		{
